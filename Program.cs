6deabce784676578a97dca4e37ae30bc3ab222e6
ktopchiev <karlostopchiev@gmail.com>
2023-11-30 19:30:20 +0200

    added functionality to check if there is existing CarList xlsx file

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Globalization;
 using OfficeOpenXml;
 using OfficeOpenXml.Style;
+using System.Security.Cryptography.X509Certificates;
+using System.Diagnostics;
 
 namespace CarChecker
 {
@@ -13,7 +15,7 @@ namespace CarChecker
         {
             HtmlWeb web = new HtmlWeb();
             var carList = new List<Car>();
-            HtmlNode next = null;
+            HtmlNode next;
             var pageCounter = 1;
 
             do
@@ -27,11 +29,13 @@ namespace CarChecker
                     foreach (var item in itemLinks)
                     {
                         var id = item.GetAttributeValue("href", "").Split("=", StringSplitOptions.RemoveEmptyEntries)[1].ToString().Trim();
-
                         var caption1 = item.ChildNodes.First(i => i.HasClass("caption1"));
                         var caption2 = item.ChildNodes.First(i => i.HasClass("caption2"));
                         var caption2a = item.ChildNodes.First(i => i.HasClass("caption2a")).InnerText.Split(" | ", StringSplitOptions.RemoveEmptyEntries);
                         var price = item.ChildNodes.First(i => i.HasClass("price"));
+                        var url = item.GetAttributeValue("href", "").ToString().Trim();
+
+                        
 
                         carList.Add(new Car
                         {
@@ -39,9 +43,10 @@ namespace CarChecker
                             Title = caption1.InnerText,
                             Description = caption2.InnerText,
                             AssemblyYear = caption2a[0],
-                            Kilometers = caption2a[1],
+                            Kilometers = int.Parse(caption2a[1].Replace("км","").Trim().Replace(" ", "")),
                             FuelType = caption2a[2],
-                            Price = price.InnerText
+                            Price = double.Parse(price.InnerText.Replace("лв.","").Trim().Replace(" ","")),
+                            Url = url
                         });
                     }
                 }
@@ -52,59 +57,77 @@ namespace CarChecker
                 }
 
                 pageCounter++;
+
             } while (next != null);
 
-            Console.WriteLine($"List of {carList.Count} cars was prepared.");
+            Console.WriteLine($"List of {carList.Count} cars has been prepared.");
 
             string filePath = "C:\\Users\\karlo\\Desktop\\Car checker\\CarList.xlsx";
 
-            //TODO: Continue from here
-            if (File.Exists(filePath))
-            {
-
-            }
+            ExcelWorksheet ws;
+            ExcelPackage excel;
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-            ExcelPackage excel = new ExcelPackage();
 
-
-            var ws = excel.Workbook.Worksheets.Add(DateTime.Now.ToString("MM/dd/yyyy HH:mm"));
-
-            //Create excel file header row based on Car class public properties
-            var props = typeof(Car).GetProperties();
-
-            for (int p = 0; p < props.Length - 1; p++)
+            //TODO: Continue from here
+            if (!File.Exists(filePath))
             {
-                ws.Cells[1, p + 1].Value = props[p].Name;
-            }
-
-            //Add data
-            for (int t = 0; t < carList.Count - 1; t++)
-            {
-                ws.Cells[t + 1, 1].Value = carList[t].Id;
-                ws.Cells[t + 1, 2].Value = carList[t].Title;
-                ws.Cells[t + 1, 3].Value = carList[t].Description;
-                ws.Cells[t + 1, 4].Value = carList[t].AssemblyYear;
-                ws.Cells[t + 1, 5].Value = carList[t].Kilometers;
-                ws.Cells[t + 1, 6].Value = carList[t].FuelType;
-                ws.Cells[t + 1, 7].Value = carList[t].Price;
-            }
-
-            //This goes up
-            if (File.Exists(filePath))
-            {
-                excel = new ExcelPackage(filePath);
-
+                excel = new ExcelPackage();
+                Console.WriteLine("New xlsx file with name CarList has been created.");
             }
             else
             {
-                FileStream fileStream = File.Create(filePath);
-                fileStream.Close();
-
-                File.WriteAllBytes(filePath, excel.GetAsByteArray());
+                excel = new ExcelPackage(filePath);
             }
 
-            excel.Dispose();
+            var today = DateTime.Now.ToString("ddMMyyyy");
 
+            if (!excel.Workbook.Worksheets.Any(s => s.Name == today))
+            {
+                ws = excel.Workbook.Worksheets.Add(today);
+                CreateHeaderRow(ws);
+                Console.WriteLine($"New worksheet named {today} has been created.");
+            }
+            else
+            {
+                ws = excel.Workbook.Worksheets[today];
+            }
+
+            AddListDataToSheet(ws, carList);
+
+            FileStream fileStream = File.Create(filePath);
+            fileStream.Close();
+            File.WriteAllBytes(filePath, excel.GetAsByteArray());
+
+            excel.Dispose();
+            Console.WriteLine("All done.");
+        }
+
+        public static void AddListDataToSheet(ExcelWorksheet ws, List<Car> carList)
+        {
+            //Add data
+            for (int t = 1; t <= carList.Count; t++)
+            {
+                ws.Cells[t + 1, 1].Value = carList[t-1].Id;
+                ws.Cells[t + 1, 2].Value = carList[t-1].Title;
+                ws.Cells[t + 1, 3].Value = carList[t-1].Description;
+                ws.Cells[t + 1, 4].Value = carList[t-1].AssemblyYear;
+                ws.Cells[t + 1, 5].Value = carList[t-1].Kilometers;
+                ws.Cells[t + 1, 6].Value = carList[t-1].FuelType;
+                ws.Cells[t + 1, 7].Value = carList[t-1].Price;
+                ws.Cells[t + 1, 8].Value = carList[t-1].Url;
+            }
+            Console.WriteLine($"Cars data was successfuly written in sheet {ws.Name}.");
+        }
+
+        public static void CreateHeaderRow(ExcelWorksheet ws)
+        {
+            //Create excel file header row based on Car class public properties
+            var props = typeof(Car).GetProperties();
+
+            for (int p = 0; p < props.Length; p++)
+            {
+                ws.Cells[1, p + 1].Value = props[p].Name;
+            }
         }
 
         public class Car
@@ -113,9 +136,10 @@ namespace CarChecker
             public string Title { get; set; }
             public string Description { get; set; }
             public string AssemblyYear { get; set; }
-            public string Kilometers { get; set; }
+            public int Kilometers { get; set; }
             public string FuelType { get; set; }
-            public string Price { get; set; }
+            public double Price { get; set; }
+            public string Url { get; set; }
         }
     }
 }
